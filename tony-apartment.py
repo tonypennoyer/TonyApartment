e392d7448308d67d29f57f9f732a67a906e5ef8a
Tony Pennoyer <tonypennoyer@gmail.com>
2023-01-07 13:20:02 -0500

    add csv and remove code

--- tony-apartment.py
+++ tony-apartment.py
@@ -11,9 +11,7 @@ import pandas as pd
 ### Laundry 5
 ### Stores 3
 
-
-d = {'mins_to_subway':[0,1],'mins_to_park':[0,1],'cost':[0,1], 'broker_fee':[0,1]'laundry':[0,1],'sq_ft':[0,1],'finish':[0,1],'stores':[0,1],'elevator':[1,0],'exposure':[0,1]}
-df = pd.DataFrame(data=d)
+df = pd.read_csv('New apt - WPM.csv')
 
 print(df)
 
